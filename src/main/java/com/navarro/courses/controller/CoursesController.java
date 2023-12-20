3f0f923066fac40121f5acf0ff8f36eb9b4fecef
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-20 13:01:28 -0300

    criaçao do metodo POST

--- src/main/java/com/navarro/courses/controller/CoursesController.java
+++ src/main/java/com/navarro/courses/controller/CoursesController.java
@@ -1,10 +1,10 @@
 package com.navarro.courses.controller;
 
+import com.navarro.courses.CoursesApplication;
 import com.navarro.courses.model.Course;
 import com.navarro.courses.repository.CourseRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +20,11 @@ public class CoursesController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course body) {
+        return courseRepository.save(body);
     }
 }
