d0816e33ab3288ee1fde42755a6c2c2ac4294ba6
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-27 17:50:07 -0300

    criando EnumValidator e aplicando no CourseDTO

--- src/main/java/com/navarro/courses/dto/CourseDTO.java
+++ src/main/java/com/navarro/courses/dto/CourseDTO.java
@@ -1,6 +1,8 @@
 package com.navarro.courses.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.navarro.courses.enums.Category;
+import com.navarro.courses.enums.validation.ValueOfEnum;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,7 +14,7 @@ import java.util.List;
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotNull @NotBlank @Size(max = 150) String name,
-        @NotNull @Size(max = 15) String category,
+        @NotNull @Size(max = 15) @ValueOfEnum(enumClass = Category.class) String category,
         @NotNull @Valid List<LessonDTO> lesson
 ) {
 }
